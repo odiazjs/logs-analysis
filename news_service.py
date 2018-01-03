fa690d494b3653bf761cb9c37f2805b0664a245c
odiaz <odiaz.dw@gmail.com>
2018-01-02 21:28:14 -0600

    get most popular authors.

--- news_service.py
+++ news_service.py
@@ -5,10 +5,27 @@ class NewsService():
         self.db_factory = db.DbConnectionFactory("news")
     
     def get_popular_articles(self, no_of_articles):
-        """Return the (no_of_articles) most popular articles sorted by popularity."""
-        query = '''SELECT a.* FROM log l
-                    JOIN articles a
-                    ON l.path = ('/article/' || a.slug) LIMIT 100'''
+        """Return the most popular articles sorted by popularity."""
+        query = '''SELECT title, count(*) as views 
+                    FROM articles a INNER JOIN log l 
+                    ON a.slug=substring(l.path, 10) 
+                    GROUP BY title 
+                    ORDER BY views DESC 
+                    LIMIT 3;'''
+        c = self.db_factory.executeQuery(query)
+        articles = c.fetchall()
+        return articles
+
+    def get_popular_authors(self, no_of_articles):
+        """Return the most popular authors sorted by popularity."""
+        query = '''SELECT at.name, count(*) as views
+                    FROM authors at INNER JOIN articles a
+                    ON at.id = a.author
+                    INNER JOIN log l 
+                    ON a.slug=substring(l.path, 10)
+                    GROUP BY name 
+                    ORDER BY views DESC 
+                    LIMIT 3;'''
         c = self.db_factory.executeQuery(query)
         articles = c.fetchall()
         return articles
--- run.py
+++ run.py
@@ -2,4 +2,9 @@ import news_service
 
 news_service = news_service.NewsService()
 articles = news_service.get_popular_articles(3)
+print('---- MOST POPULAR ARTICLES ----')
 print(articles)
+print('---- MOST POPULAR AUTHORS ----')
+authors = news_service.get_popular_authors(3)
+print(authors)
+
